e2a8f28c6136b576d491be17d274170cf3f84b4b
lsshormi <l.s.shormi@gmail.com>
2024-03-16 21:59:34 +0600

    initializing database

--- corporate/asset_tracking/admin.py
+++ corporate/asset_tracking/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Company, Employee, Device
 
 # Register your models here.
+
+admin.site.register(Company)
+admin.site.register(Employee)
+admin.site.register(Device)
--- corporate/asset_tracking/models.py
+++ corporate/asset_tracking/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Employee(models.Model):
+    name = models.CharField(max_length=100)
+    company = models.ForeignKey(Company, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+class Device(models.Model):
+    TYPE_CHOICES = (
+        ('phone', 'Phone'),
+        ('tablet', 'Tablet'),
+        ('laptop', 'Laptop'),
+        ('desktop', 'Desktop'),
+        ('other', 'Other'),
+        
+    )
+
+    condition = models.CharField(max_length=100)
+    type = models.CharField(max_length=100, choices=TYPE_CHOICES)
+    checked_out = models.BooleanField(default=False)
+    checked_out_date = models.DateTimeField(null=True, blank=True)
+    return_date = models.DateTimeField(null=True, blank=True)
+    assigned_to = models.ForeignKey(Employee, on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.type} - {self.id}"
